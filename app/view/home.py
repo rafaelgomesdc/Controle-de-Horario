699c02c49a3ff21b5fd9f66aa4fe7f4164724157
rafaelgomesdc <rafaelgomesdc0000@gmail.com>
2026-04-07 09:05:54 -0300

    feat: timer

--- app/view/home.py
+++ app/view/home.py
@@ -4,24 +4,26 @@ import asyncio
 class Home_view():
     def __init__(self, page: ft.Page):
         self.page = page
-        self.running = False;
+        self.running = False
         self.time = 8 * 3600
 
     def build(self):
-        self.ui_clock = ft.Text(value="08:00:00")
-        self.ui_bt_clock = ft.ElevatedButton("CLOCK", on_click=self.Clock_Start)
+        self.ui_clock = ft.Text(value="00:00:00", size=20)
+        self.ui_timer = ft.Text(value="08:00:00", size=40)
+        self.ui_bt_timer = ft.ElevatedButton("CLOCK", on_click=self.timer_to_active)
         self.ui_config_button = ft.ElevatedButton("CONFIG", on_click=self.go_config)
         return ft.Column(
             [
                 self.ui_clock,
-                self.ui_bt_clock,
+                self.ui_timer,
+                self.ui_bt_timer,
                 self.ui_config_button
             ],
             alignment=ft.MainAxisAlignment.CENTER,
             horizontal_alignment=ft.CrossAxisAlignment.CENTER
         )
 
-    async def Run_Clock(self):
+    async def run_timer(self):
         while self.running:
             self.time -= 1
 
@@ -29,19 +31,21 @@ class Home_view():
             m = (self.time % 3600) // 60
             s = self.time % 60
 
-            self.ui_clock.value = f"{h:02}:{m:02}:{s:02}"
+            self.ui_timer.value = f"{h:02}:{m:02}:{s:02}"
             self.page.update()
             await asyncio.sleep(1)
+
+    async def clock(self):
+        while True:
+            h = 0
+            m = 0
+            s = 0
     
-    def Clock_Start(self, e):
+    def timer_to_active(self, e):
         if not self.running:
             self.running = True
             self.page.run_task(self.Run_Clock)
         elif self.running:
-            self.running = False
-    
-    def Clock_Stop(self, e):
-        if self.running:
             self.running = False
 
     async def go_config(self, e):
